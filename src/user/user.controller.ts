5546121c8e2310b8c986b83afb7296fd4679e51d
sahatovbatyr <sahatovbatyr@gmail.com>
2025-03-21 00:16:31 +0300

    added: getByUsername, getByEmail , updateRoles , updateEmail, updatePassword methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserCreateDto } from './dto/user.create.dto';
+import { plainToInstance } from 'class-transformer';
+import { UserResponseDto } from './dto/user.response.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {
+  }
+
+  @Post('create')
+  async create(@Body() userDto: UserCreateDto) {
+    const user = this.userService.create(userDto);
+    const userRes = plainToInstance(UserResponseDto, user);
+    return userRes;
+  }
+
+  @Get('get-by-id/:id')
+  async getById(@Param('id') id: number): Promise<UserResponseDto> {
+    const user = await this.userService.findById_orThrow(id);
+    return plainToInstance(UserResponseDto, user);
+
+  }
+
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { AbsBaseModelService } from '../common/AbsBaseModelService';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -6,6 +6,10 @@ import { Repository } from 'typeorm';
 import { UserCreateDto } from './dto/user.create.dto';
 import { RoleService } from '../role/role.service';
 import { RolesEnum } from '../enums/roles.enum';
+import * as bcrypt from 'bcrypt';
+import { UserUpdateRolesDto } from './dto/user.update-roles.dto';
+import { UserUpdateEmailDto } from './dto/user.update-email.dto';
+import { UserUpdatePasswordDto } from './dto/user.update-password.dto';
 
 @Injectable()
 export class UserService extends AbsBaseModelService<User> {
@@ -15,7 +19,7 @@ export class UserService extends AbsBaseModelService<User> {
     private readonly userRepository: Repository<User>,
     private readonly roleService: RoleService,
   ) {
-    super(userRepository);
+    super(userRepository, User.name);
   }
 
   protected getRelations(): string[] {
@@ -37,8 +41,112 @@ export class UserService extends AbsBaseModelService<User> {
       throw new BadRequestException(`Error. user with email: ${userDto.email} already exists.`);
     }
 
+    const saltOrRounds = 10;
+    const hash = await bcrypt.hash(userDto.password, saltOrRounds);
+    userDto.password = hash;
+
     userDto.roles = [await this.roleService.findOneByProp_orThrow('title', RolesEnum.USER)];
-    return super.create(userDto);
+    return await super.create(userDto);
+  }
+
+  public async getByUsername(username: string): Promise<User | null> {
+    return await super.findOneByProp('username', username);
+
+  }
+
+  public async getByUsername_orThrow(username: string): Promise<User> {
+    return await super.findOneByProp_orThrow('username', username);
+
+  }
+
+  public async getByEmail(email: string): Promise<User | null> {
+    return await super.findOneByProp('email', email);
+
+  }
+
+  public async getByEmail_orThrow(email: string): Promise<User> {
+    return await super.findOneByProp_orThrow('email', email);
+
+  }
+
+  async updateRoles(userDto: Readonly<UserUpdateRolesDto>): Promise<User> {
+    if (userDto.roleIdList.length == 0) {
+      throw new BadRequestException('Specify the users roles!');
+    }
+
+    const user = await super.findById_orThrow(userDto.id);
+    const roles = await this.roleService.getRolesByIdList(userDto.roleIdList);
+
+    if (roles.length != userDto.roleIdList.length) {
+      throw new BadRequestException('Incorrect roles.');
+    }
+
+    user.roles = roles;
+    const res = await this.userRepository.save(user);
+    // this.logger.log(` User id:${userDto.id} roles updated`);
+
+    return res;
+  }
+
+  async updateEmail(
+    userDto: Readonly<UserUpdateEmailDto>,
+    author: string,
+  ): Promise<User> {
+    if (author !== userDto.username) {
+      throw new ForbiddenException('Access Denied. The email can be changed only by the owner.');
+    }
+
+    const user = await this.getByUsername_orThrow(userDto.username);
+    const userByEmail = await this.getByEmail(userDto.email);
+
+    if (userByEmail) {
+      throw new BadRequestException(`Email ${userDto.email} already exists.`);
+    }
+    user.email = userDto.email;
+    const res = await this.userRepository.save(user);
+    return res;
+  }
+
+
+  async comparePassword(unhashedPassword: string, hashedPassword: string) {
+    return await bcrypt.compare(unhashedPassword, hashedPassword);
+  }
+
+  async updatePassword(userDto: UserUpdatePasswordDto, author: string) {
+    if (author !== userDto.username) {
+      throw new ForbiddenException(
+        'Access Denied. The password can be changed only by the owner.',
+      );
+    }
+    // this.logger.log(`trying updatePassword() for user: ${userDto.username}`);
+
+    if (userDto.newPassword != userDto.newPasswordConfirmation) {
+      throw new BadRequestException('new Password and confirmation not match.');
+    }
+
+    const hashedOldPassword = await bcrypt.hash(userDto.oldPassword, 10);
+
+    const user = await this.userRepository.findOne({
+      where: { username: userDto.username },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Username or password are wrong not match.');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      hashedOldPassword,
+      user.password,
+    );
+
+    if (isPasswordValid) {
+      throw new NotFoundException('Username or password are wrong not match.');
+    }
+
+    user.password = await bcrypt.hash(userDto.newPassword, 10);
+    await this.userRepository.save(user);
+    // this.logger.log(`User: ${user.username} password changed.`);
+    return;
   }
 
 
