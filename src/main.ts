436d972a045f554034b69e2e65d6dbace58c5c06
sahatovbatyr <sahatovbatyr@gmail.com>
2025-03-29 00:07:20 +0300

    added: added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AppEnvInterface } from './config/AppEnvInterface';
 import { EnvKeysEnum } from './enums/EnvKeysEnum';
+import { CustomValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,8 +11,14 @@ async function bootstrap() {
 
   const port: number = configService.get<number>(EnvKeysEnum.HOST_PORT) ?? 3000;
   app.setGlobalPrefix('api');
+
+  const validationPipe = new CustomValidationPipe();
+  app.useGlobalPipes(validationPipe);
+
+
   await app.listen(port);
   console.log(`Server started on port:${port}`);
+
 
 }
 
